89ef503a86aeaa7a79a45e82f9a3998668803584
leotinoco7 <leofptinoco7@gmail.com>
2022-05-25 20:46:13 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Validation
   app.useGlobalPipes(new ValidationPipe());
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('PizzaFresh')
     .setDescription('Aplicação para gestão das mesas de uma pizzaria')
     .setVersion('1.0.0')
     .addTag('status')
     .addTag('table')
+    .addTag('product')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
@@ -21,4 +24,5 @@ async function bootstrap() {
 
   await app.listen(3333);
 }
+
 bootstrap();
