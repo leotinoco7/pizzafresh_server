1609b2c6c69cecffc58fb5432536442cd8f3d650
leotinoco7 <leofptinoco7@gmail.com>
2022-05-25 20:46:16 -0300

    swagger

---
+++ src/product/product.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
+  }
+
+  async findById(id: string): Promise<Product> {
+    const record = await this.prisma.product.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Product> {
+    return this.findById(id);
+  }
+
+  create(dto: CreateProductDto): Promise<Product> {
+    const data: Product = { ...dto };
+
+    return this.prisma.product.create({ data }).catch(this.handleError);
+  }
+
+  async update(id: string, dto: UpdateProductDto): Promise<Product> {
+    await this.findById(id);
+
+    const data: Partial<Product> = { ...dto };
+
+    return this.prisma.product.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.product.delete({ where: { id } });
+  }
+
+  handleError(error: Error) {
+    console.log(error.message);
+
+    return undefined;
+  }
+}
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './dto/create-table.dto';
 import { UpdateTableDto } from './dto/update-table.dto';
@@ -12,17 +12,29 @@ export class TableService {
     return this.prisma.table.findMany();
   }
 
-  findOne(id: string): Promise<Table> {
-    return this.prisma.table.findUnique({ where: { id } });
+  async findById(id: string): Promise<Table> {
+    const record = await this.prisma.table.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Table> {
+    return this.findById(id);
   }
 
   create(dto: CreateTableDto): Promise<Table> {
     const data: Table = { ...dto };
 
-    return this.prisma.table.create({ data });
+    return this.prisma.table.create({ data }).catch(this.handleError);
   }
 
-  update(id: string, dto: UpdateTableDto): Promise<Table> {
+  async update(id: string, dto: UpdateTableDto): Promise<Table> {
+    await this.findById(id);
+
     const data: Partial<Table> = { ...dto };
 
     return this.prisma.table.update({
@@ -32,6 +44,14 @@ export class TableService {
   }
 
   async delete(id: string) {
+    await this.findById(id);
+
     await this.prisma.table.delete({ where: { id } });
+  }
+
+  handleError(error: Error) {
+    console.log(error.message);
+
+    return undefined;
   }
 }
